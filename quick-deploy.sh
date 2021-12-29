76707076faf2f962ca0b248fb41491e80d155a5b
Nick <nicholaszaslavsky@gmail.com>
2021-12-29 16:48:47 -0500

    fixes shell script wait section

--- quick-deploy.sh
+++ quick-deploy.sh
@@ -1,5 +1,5 @@
 kubectl apply -f setup/
-wait 5 #The setup folder needs to be applied first because it has the prerequisite namespaces for all other folders
+sleep 5 #The setup folder needs to be applied first because it has the prerequisite namespaces for all other folders
 kubectl apply -f loki-promtail/
 kubectl apply -f test-target/
 kubectl apply -f grafana-dashboard/
